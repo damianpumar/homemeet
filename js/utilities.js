2c620137118e9ea4a3eac43c68b45ef2369d5487
Damián Pumar <damianpumar@gmail.com>
2020-03-27 09:07:21 +0100

    Improve content

--- js/utilities.js
+++ js/utilities.js
@@ -1,3 +1,17 @@
+$(window).load(function() {
+	showDeveloperMessage();
+	showDaysInQuarantine();
+});
+
+function showDaysInQuarantine() {
+	const oneDay = 1000 * 3600 * 24;
+	const startQuarantine = new Date(2020, 3, 13, 0, 0);
+	const now = new Date();
+	const daysDifference = (now.getTime() - startQuarantine.getTime()) / oneDay;
+	const daysInQuarantine = Math.abs(Math.trunc(daysDifference));
+	$("#daysInQuarantine").text(daysInQuarantine);
+}
+
 function showDeveloperMessage() {
 	console.clear();
 	console.log("Sorry, I cleared console because I wanted show you this message below");
@@ -31,7 +45,3 @@ function loadAsync() {
 }
 
 loadAsync();
-
-$(window).load(function() {
-	showDeveloperMessage();
-});
