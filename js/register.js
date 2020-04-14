9df5f9fb4350b65bf29aab80b1acaa46022ac1be
Damián Pumar <damianpumar@gmail.com>
2020-04-14 21:09:10 +0200

    Change email sender

--- js/register.js
+++ js/register.js
@@ -78,7 +78,8 @@ $(document).ready(function ($) {
 			url: $(this).attr("action"),
 			type: $(this).attr("method"),
 			crossDomain: true,
-			data: $(this).serialize(),
+			contentType: "application/json; charset=utf-8",
+			data: JSON.stringify($(this).serializeObject()),
 			success: function () {
 				throwRegisterSuccessSubmit();
 				saveUserData();
@@ -92,7 +93,7 @@ $(document).ready(function ($) {
 					registerButton.html("Register");
 					registerButton.dequeue();
 				});
-			}
+			},
 		});
 
 		return false;
@@ -130,3 +131,19 @@ function refreshUserStatus() {
 		email.val(userEmail);
 	}
 }
+
+$.fn.serializeObject = function () {
+	var o = {};
+	var a = this.serializeArray();
+	$.each(a, function () {
+		if (o[this.name] !== undefined) {
+			if (!o[this.name].push) {
+				o[this.name] = [o[this.name]];
+			}
+			o[this.name].push(this.value || "");
+		} else {
+			o[this.name] = this.value || "";
+		}
+	});
+	return o;
+};
